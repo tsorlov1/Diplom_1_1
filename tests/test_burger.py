67098cb976e2e95a9f5405d0c7dbf4667a3536cd
Tatyana Korneeva <tsorlov1@yandex.ru>
2024-06-24 15:45:01 +0300

    Diplom_1

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -1,8 +1,6 @@
 import pytest
 from unittest.mock import Mock
-from praktikum.bun import Bun
 from praktikum.burger import Burger
-from praktikum.ingredient import Ingredient
 from data import DataBun, DataIngredient
 from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
 
@@ -10,10 +8,12 @@ from praktikum.ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FI
 class TestBurger:
 
     def test_set_buns_burger(self):
-        bun = Bun(DataBun.BLACK_BUN, DataBun.BLACK_BUN_PRICE)
         burger = Burger()
-        burger.set_buns(bun)
-        assert burger.bun == bun
+        mock_bun = Mock()
+        mock_bun.get_name.return_value = DataBun.BLACK_BUN
+        mock_bun.get_price.return_value = DataBun.BLACK_BUN_PRICE
+        burger.set_buns(mock_bun)
+        assert burger.bun == mock_bun
 
     @pytest.mark.parametrize("ingredient_type, name, price", [
         (INGREDIENT_TYPE_SAUCE, DataIngredient.INGREDIENT_NAME_HOT_SAUCE,
@@ -23,39 +23,38 @@ class TestBurger:
     ])
     def test_add_ingredient_burger(self, ingredient_type, name, price):
         burger = Burger()
-        ingredient = Ingredient(ingredient_type, name, price)
-        burger.add_ingredient(ingredient)
+        mock_ingredient = Mock()
+        mock_ingredient.get_name.return_value = name
+        burger.add_ingredient(mock_ingredient)
         assert burger.ingredients[0].get_name() == name
 
     def test_remove_ingredient_burger(self):
         burger = Burger()
-        ingredient = Ingredient(INGREDIENT_TYPE_SAUCE, DataIngredient.INGREDIENT_NAME_HOT_SAUCE,
-                                DataIngredient.INGREDIENT_PRICE_HOT_SAUCE)
-        burger.add_ingredient(ingredient)
+        mock_ingredient = Mock()
+        burger.add_ingredient(mock_ingredient)
         burger.remove_ingredient(0)
         assert len(burger.ingredients) == 0
 
     def test_move_ingredient_burger(self):
         burger = Burger()
-        ingredient_one = Ingredient(INGREDIENT_TYPE_SAUCE, DataIngredient.INGREDIENT_NAME_HOT_SAUCE,
-                                    DataIngredient.INGREDIENT_PRICE_HOT_SAUCE)
-        ingredient_two = Ingredient(INGREDIENT_TYPE_FILLING, DataIngredient.INGREDIENT_NAME_CHILI_CUTLET,
-                                    DataIngredient.INGREDIENT_PRICE_CHILI_CUTLET)
-        burger.add_ingredient(ingredient_one)
-        burger.add_ingredient(ingredient_two)
+        mock_ingredient_one = Mock()
+        mock_ingredient_two = Mock()
+        burger.add_ingredient(mock_ingredient_one)
+        burger.add_ingredient(mock_ingredient_two)
         burger.move_ingredient(0, 1)
-        assert burger.ingredients == [ingredient_two, ingredient_one]
+        assert burger.ingredients == [mock_ingredient_two, mock_ingredient_one]
 
     def test_get_price_burger(self):
         burger = Burger()
-        bun = Bun(DataBun.BLACK_BUN, DataBun.BLACK_BUN_PRICE)
-        ingredient_one = Ingredient(INGREDIENT_TYPE_SAUCE, DataIngredient.INGREDIENT_NAME_HOT_SAUCE,
-                                    DataIngredient.INGREDIENT_PRICE_HOT_SAUCE)
-        ingredient_two = Ingredient(INGREDIENT_TYPE_FILLING, DataIngredient.INGREDIENT_NAME_CHILI_CUTLET,
-                                    DataIngredient.INGREDIENT_PRICE_CHILI_CUTLET)
-        burger.set_buns(bun)
-        burger.add_ingredient(ingredient_one)
-        burger.add_ingredient(ingredient_two)
+        mock_bun = Mock()
+        mock_bun.get_price.return_value = DataBun.BLACK_BUN_PRICE
+        mock_ingredient_one = Mock()
+        mock_ingredient_one.get_price.return_value = 100
+        mock_ingredient_two = Mock()
+        mock_ingredient_two.get_price.return_value = 100
+        burger.set_buns(mock_bun)
+        burger.add_ingredient(mock_ingredient_one)
+        burger.add_ingredient(mock_ingredient_two)
         assert burger.get_price() == 400
 
     def test_get_receipt_burger(self):
@@ -65,12 +64,12 @@ class TestBurger:
         mock_bun.get_price.return_value = DataBun.BLACK_BUN_PRICE
 
         mock_ingredient_one = Mock()
-        mock_ingredient_one.get_type.return_value = INGREDIENT_TYPE_FILLING
+        mock_ingredient_one.get_type.return_value = DataIngredient.INGREDIENT_TYPE_FILLING
         mock_ingredient_one.get_name.return_value = DataIngredient.INGREDIENT_NAME_CHILI_CUTLET
         mock_ingredient_one.get_price.return_value = DataIngredient.INGREDIENT_PRICE_CHILI_CUTLET
 
         mock_ingredient_two = Mock()
-        mock_ingredient_two.get_type.return_value = INGREDIENT_TYPE_SAUCE
+        mock_ingredient_two.get_type.return_value = DataIngredient.INGREDIENT_TYPE_SAUCE
         mock_ingredient_two.get_name.return_value = DataIngredient.INGREDIENT_NAME_HOT_SAUCE
         mock_ingredient_two.get_price.return_value = DataIngredient.INGREDIENT_PRICE_HOT_SAUCE
 
